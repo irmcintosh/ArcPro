ad199465c6bc195ae60c12b3efee1f7ea3115146
Iain MacIntosh <35180016+irmcintosh@users.noreply.github.com>
2025-12-08 20:32:48 -0500

    Refactor variables and conditionals in weather script

--- WOE/landslideCombinedWeather.js
+++ WOE/landslideCombinedWeather.js
@@ -1,7 +1,7 @@
-var a = $feature.landslide_rainrate_effect;
-var b = $feature.landslide_rain24_effect;
-var c = $feature.landslide_snowmelt_effect;
+var a = $feature.landsliderainfallrate
+var b =$feature.rain24_accum;
+var c = $feature.snowmelt24;
 
 If (a == "F" || b == "F" || c == "F") return "F";
-ElseIf (a == "M" || b == "M" || c == "M") return "M";
+Else If (a == "M" || b == "M" || c == "M") return "M";
 Else return "U";
