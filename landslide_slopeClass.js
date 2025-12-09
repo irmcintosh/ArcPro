f98a6734410908d118db902afa5b62ec4cf8ffc7
Iain MacIntosh <35180016+irmcintosh@users.noreply.github.com>
2025-12-08 20:17:46 -0500

    Delete landslide_slopeClass.js

--- landslide_slopeClass.js
+++
@@ -1,6 +0,0 @@
-var d = $feature.slope_deg;
-If (d <= 10) return "Flat";
-ElseIf (d <= 20) return "LowRisk";
-ElseIf (d <= 30) return "Moderate";
-ElseIf (d <= 35) return "High";
-Else return "Extreme";
